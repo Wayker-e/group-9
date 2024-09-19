fb753d92ed3c7fa98e6607f6a3e64f95b5622d3e
tveit <andredt@stud.ntnu.no>
2024-09-19 17:46:38 +0200

    Andreas til Heidi

--- Andreas code.py
+++ Andreas code.py
@@ -59,3 +59,31 @@ def newton_raphson(lines, buses, minmax_gen, loads):
     # Find the calculated values
     print(x)
     return
+
+
+
+Y = create_Ybus(line_data_fig2)
+
+v1 = 1.05
+v2 = 1.04
+v3 = 0.9059
+d1 = 0
+d2 = -0.023589
+d3 = -0.119510
+
+
+P2 = (v2*v1*abs(Y[1][0])*np.cos(np.angle(Y[1][0])-d2+d1)
+    + v2*v2*abs(Y[1][1])*np.cos(np.angle(Y[1][1])-d2+d2)
+    + v2*v3*abs(Y[1][2])*np.cos(np.angle(Y[1][2])-d2+d3))
+
+P3 = (v3*v1*abs(Y[2][0])*np.cos(np.angle(Y[2][0])-d3+d1)
+    + v3*v2*abs(Y[2][1])*np.cos(np.angle(Y[2][1])-d3+d2)
+    + v3*v3*abs(Y[2][2])*np.cos(np.angle(Y[2][2])-d3+d3))
+
+Q2 =-(v2*v1*abs(Y[1][0])*np.sin(np.angle(Y[1][0])-d2+d1)
+    + v2*v2*abs(Y[1][1])*np.sin(np.angle(Y[1][1])-d2+d2)
+    + v2*v3*abs(Y[1][2])*np.sin(np.angle(Y[1][2])-d2+d3))
+
+Q3 =-(v3*v1*abs(Y[2][0])*np.sin(np.angle(Y[2][0])-d3+d1)
+    + v3*v2*abs(Y[2][1])*np.sin(np.angle(Y[2][1])-d3+d2)
+    + v3*v3*abs(Y[2][2])*np.sin(np.angle(Y[2][2])-d3+d3))
